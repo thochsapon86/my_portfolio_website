dfd5ef3c6bd248b33d41c75d5a1d211de0ddcc4f
thochsapon86 <145741685+thochsapon86@users.noreply.github.com>
2023-10-07 22:06:28 +0700

    Update&Fix
    Update: Education section.
    Fix: add more CSS.

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
-function scrollToID() {
-    var access = document.getElementById('#about-me');
-    access.scrollIntoView({behavior: 'smooth'}, true);
-}
+function aboutMe() {
+    var home = document.getElementById('#Home');
+    var about_me = document.getElementById('#AbouMe');
+    var edu = document.getElementById('#Education');
+    var prj = document.getElementById('#Project');
+    var cer = document.getElementById('#Certificate');
+
+    home.classList.remove("active");
+    edu.classList.remove("active");
+    prj.classList.remove("active");
+    cer.classList.remove("active");
+    
+    about_me.classList.add("active");
+};
